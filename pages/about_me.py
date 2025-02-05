e00268d69472fac4d281bdd727410cfbd0d393bc
mberriosmoreno <94083004+mberriosmoreno@users.noreply.github.com>
2025-02-05 16:17:02 -0600

    Create about_me.py

---
+++ pages/about_me.py
@@ -0,0 +1,8 @@
+import streamlit as st
+
+def main():
+    st.title("🏠 Acerca de Mí")
+    st.write("""
+    Esta es la página "Acerca de Mí". Aquí puedes incluir información sobre ti,
+    tu proyecto o cualquier otro detalle relevante.
+    """)
